3664d04da5cd58d0ef832f5b028daee87b0438ad
MagicTable <13459588094@163.com>
2025-06-08 18:04:13 +0800

    update note

---
+++ assets/classDiagram-MKYM2BOE-Jaag2mB-.js
@@ -0,0 +1,1 @@
+import{w as a,Y as s,k as t,u as e}from"./chunk-SHLHLJRN-CxaE9wtL.js";import"./chunk-IO347I67-D1DN5oaW.js";import{m as o}from"./mermaid.esm.min-BBzbvsWd.js";import"./app-D-KDpy-e.js";var p={parser:t,get db(){return new e},renderer:s,styles:a,init:o(r=>{r.class||(r.class={}),r.class.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{p as diagram};
