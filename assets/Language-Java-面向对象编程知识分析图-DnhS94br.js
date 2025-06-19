36fe7ed69e2e6982ca1dea875c9b2dee79598efe
MagicTable <13459588094@163.com>
2025-06-19 17:39:10 +0800

    update note

---
+++ assets/Language-Java-面向对象编程知识分析图-DnhS94br.js
@@ -0,0 +1,1 @@
+const E="/assets/Language-Java-%E9%9D%A2%E5%90%91%E5%AF%B9%E8%B1%A1%E7%BC%96%E7%A8%8B%E7%9F%A5%E8%AF%86%E5%88%86%E6%9E%90%E5%9B%BE-DIqwynCY.png";export{E as _};
---
+++ assets/Language-Markdown-热门推荐-模板-联系人.html-BDrR3VGe.js
@@ -0,0 +1,25 @@
+import{_ as a,c as e,a as t,o as s}from"./app-B6exzHpV.js";const l={};function i(o,n){return s(),e("div",null,n[0]||(n[0]=[t(`<p><strong>文章摘要：</strong> Markdown模板展示，管理与个人生活紧密相关的人物信息，并对其进行分类整理。</p><div class="language-txt line-numbers-mode" data-highlighter="prismjs" data-ext="txt"><pre><code class="language-txt"><span class="line">---</span>
+<span class="line">昵称: &quot;&lt;网络名称/别名/假名&gt;&quot;</span>
+<span class="line">分类: &quot;&lt;家人，老乡，老师，朋友，同学，公司，客户，陌生人&gt;&quot;</span>
+<span class="line">关系: &quot;&lt;爸爸，妈妈，老婆，老公，妹妹，同事，客户等&gt;&quot;</span>
+<span class="line">头像: &quot;&lt;头像图片链接&gt;&quot;</span>
+<span class="line">姓名: &quot;&lt;姓名，未知&gt;&quot;</span>
+<span class="line">性别: &quot;&lt;男，女，未知&gt;&quot;</span>
+<span class="line">生日: &lt;XXXX-XX-XX，未知&gt;</span>
+<span class="line">国籍: &quot;&lt;国籍，未知&gt;&quot;</span>
+<span class="line">学历: &quot;&lt;学历，未知&gt;&quot;</span>
+<span class="line">户籍地址: &quot;&lt;地址，未知&gt;&quot;</span>
+<span class="line">联系方式-现住地址: &quot;&lt;地址，未知&gt;&quot;</span>
+<span class="line">联系方式-电话号码:</span>
+<span class="line">  - &quot;&lt;电话号码，未知&gt;&quot;</span>
+<span class="line">联系方式-电子邮箱:</span>
+<span class="line">  - &quot;&lt;电子邮箱，未知&gt;&quot;</span>
+<span class="line">工作单位: &quot;&lt;工作单位，暂无，未知&gt;&quot;</span>
+<span class="line">职业类别: &quot;&lt;自由职业，学生，未知&gt;&quot;</span>
+<span class="line">---</span>
+<span class="line"></span>
+<span class="line">## 约定</span>
+<span class="line"></span>
+<span class="line">## 事件</span>
+<span class="line"></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2)]))}const c=a(l,[["render",i]]),p=JSON.parse('{"path":"/software/Language-Markdown-%E7%83%AD%E9%97%A8%E6%8E%A8%E8%8D%90-%E6%A8%A1%E6%9D%BF-%E8%81%94%E7%B3%BB%E4%BA%BA.html","title":"Language - Markdown - 热门推荐 - 模板 - 联系人","lang":"zh-CN","frontmatter":{"title":"Language - Markdown - 热门推荐 - 模板 - 联系人","author":"魔术桌","cover":"","category":["软件程序/语言"],"date":"2025-06-09T00:00:00.000Z","tag":["进度-已停滞","知识总结"],"description":"文章摘要： Markdown模板展示，管理与个人生活紧密相关的人物信息，并对其进行分类整理。","head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"Language - Markdown - 热门推荐 - 模板 - 联系人\\",\\"image\\":[\\"\\"],\\"datePublished\\":\\"2025-06-09T00:00:00.000Z\\",\\"dateModified\\":\\"2025-06-15T06:48:27.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"魔术桌\\"}]}"],["meta",{"property":"og:url","content":"https://blog.magictable.ha.cn/software/Language-Markdown-%E7%83%AD%E9%97%A8%E6%8E%A8%E8%8D%90-%E6%A8%A1%E6%9D%BF-%E8%81%94%E7%B3%BB%E4%BA%BA.html"}],["meta",{"property":"og:site_name","content":"魔术桌的知识仓库"}],["meta",{"property":"og:title","content":"Language - Markdown - 热门推荐 - 模板 - 联系人"}],["meta",{"property":"og:description","content":"文章摘要： Markdown模板展示，管理与个人生活紧密相关的人物信息，并对其进行分类整理。"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2025-06-15T06:48:27.000Z"}],["meta",{"property":"article:author","content":"魔术桌"}],["meta",{"property":"article:tag","content":"知识总结"}],["meta",{"property":"article:tag","content":"进度-已停滞"}],["meta",{"property":"article:published_time","content":"2025-06-09T00:00:00.000Z"}],["meta",{"property":"article:modified_time","content":"2025-06-15T06:48:27.000Z"}]]},"git":{"updatedTime":1749970107000,"contributors":[{"name":"MagicTable","username":"","email":"13459588094@163.com","commits":2}],"changelog":[{"hash":"6642dbb9f0eb9b74560fa15a4659a0540aa8be44","time":1749970107000,"email":"13459588094@163.com","author":"MagicTable","message":"Obsidan Git: 2025-06-15 14:48:26"},{"hash":"9f294c9a014a76e309c75949ae8a8cdc22b54f10","time":1749900766000,"email":"13459588094@163.com","author":"MagicTable","message":"内容更新"}]},"autoDesc":true,"filePathRelative":"software/Language-Markdown-热门推荐-模板-联系人.md","excerpt":"<p><strong>文章摘要：</strong> Markdown模板展示，管理与个人生活紧密相关的人物信息，并对其进行分类整理。</p>\\n"}');export{c as comp,p as data};
