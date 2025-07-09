0ba0d142d5fa1c7469d4e4776be6304275a72212
MagicTable <13459588094@163.com>
2025-07-09 13:04:23 +0800

    update note

---
+++ assets/Library-Npm-Ionic8-问题总结-集成Vue3.html-CZJYzyW9.js
@@ -0,0 +1,26 @@
+import{_ as e,c as s,a,o as i}from"./app-BDRGpUI0.js";const t={};function l(c,n){return i(),s("div",null,n[0]||(n[0]=[a(`<p><strong>文章摘要：</strong> 集成Vue3库。</p><h2 id="简介" tabindex="-1"><a class="header-anchor" href="#简介"><span>简介</span></a></h2><h2 id="目录结构" tabindex="-1"><a class="header-anchor" href="#目录结构"><span>目录结构</span></a></h2><div class="language-txt line-numbers-mode" data-highlighter="prismjs" data-ext="txt"><pre><code class="language-txt"><span class="line">H5项目目录/</span>
+<span class="line">	|-- dist/    # 编译后的web文件</span>
+<span class="line">		|-- assets/         # 编译后的资源文件</span>
+<span class="line">		favicon.png         # 图标</span>
+<span class="line">		index.html          # 启动文件</span>
+<span class="line">	|-- node_modules/  # npm依赖文件</span>
+<span class="line">	|-- public/        # 静态文件</span>
+<span class="line">	|-- src/           # web源码文件</span>
+<span class="line">	</span>
+<span class="line">	.browserslistrc</span>
+<span class="line">	.eslintignore</span>
+<span class="line">	.eslintrc.cjs</span>
+<span class="line">	capacitor.config.ts</span>
+<span class="line">	cypress.config.ts</span>
+<span class="line">	index.html    # 程序根文件</span>
+<span class="line">	ionic.config.json</span>
+<span class="line">	package-lock.json    # npm依赖版本控制文件</span>
+<span class="line">	package.json         # npm配置文件</span>
+<span class="line">	tsconfig.json</span>
+<span class="line">	tsconfig.node.json</span>
+<span class="line">	vite.config.ts       # vite构建工具配置文件</span>
+<span class="line"></span>
+<span class="line">	|-- android  # 安卓项目目录</span>
+<span class="line"></span>
+<span class="line">	|-- ios      # 苹果项目目录</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="生命周期" tabindex="-1"><a class="header-anchor" href="#生命周期"><span>生命周期</span></a></h2>`,5)]))}const p=e(t,[["render",l]]),o=JSON.parse('{"path":"/software/Library-Npm-Ionic8-%E9%97%AE%E9%A2%98%E6%80%BB%E7%BB%93-%E9%9B%86%E6%88%90Vue3.html","title":"Library - Npm - Ionic 8 - 问题总结 - 集成Vue3库","lang":"zh-CN","frontmatter":{"title":"Library - Npm - Ionic 8 - 问题总结 - 集成Vue3库","author":"魔术桌","cover":"","category":["软件程序/类库"],"date":"2024-10-25T00:00:00.000Z","tag":["进度-待完善","问题总结"],"description":"文章摘要： 集成Vue3库。 简介 目录结构 生命周期","head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"Library - Npm - Ionic 8 - 问题总结 - 集成Vue3库\\",\\"image\\":[\\"\\"],\\"datePublished\\":\\"2024-10-25T00:00:00.000Z\\",\\"dateModified\\":\\"2025-06-14T11:32:46.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"魔术桌\\"}]}"],["meta",{"property":"og:url","content":"https://blog.magictable.ha.cn/software/Library-Npm-Ionic8-%E9%97%AE%E9%A2%98%E6%80%BB%E7%BB%93-%E9%9B%86%E6%88%90Vue3.html"}],["meta",{"property":"og:site_name","content":"魔术桌的知识仓库"}],["meta",{"property":"og:title","content":"Library - Npm - Ionic 8 - 问题总结 - 集成Vue3库"}],["meta",{"property":"og:description","content":"文章摘要： 集成Vue3库。 简介 目录结构 生命周期"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2025-06-14T11:32:46.000Z"}],["meta",{"property":"article:author","content":"魔术桌"}],["meta",{"property":"article:tag","content":"问题总结"}],["meta",{"property":"article:tag","content":"进度-待完善"}],["meta",{"property":"article:published_time","content":"2024-10-25T00:00:00.000Z"}],["meta",{"property":"article:modified_time","content":"2025-06-14T11:32:46.000Z"}]]},"git":{"updatedTime":1749900766000,"contributors":[{"name":"MagicTable","username":"","email":"13459588094@163.com","commits":1}],"changelog":[{"hash":"9f294c9a014a76e309c75949ae8a8cdc22b54f10","time":1749900766000,"email":"13459588094@163.com","author":"MagicTable","message":"内容更新"}]},"autoDesc":true,"filePathRelative":"software/Library-Npm-Ionic8-问题总结-集成Vue3.md","excerpt":"<p><strong>文章摘要：</strong> 集成Vue3库。</p>\\n"}');export{p as comp,o as data};
